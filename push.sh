2b84b5c848c14b6067680ae6c5a5abea83dadf03
knqyf263 <knqyf263@gmail.com>
2022-06-06 09:57:47 +0300

    Add fluentd-multiple-lockfiles

--- push.sh
+++ push.sh
@@ -7,6 +7,7 @@ fi
 
 TEST_IMAGE=ghcr.io/aquasecurity/trivy-test-images
 
+
 cat <<EOS
 
 ######################
@@ -16,6 +17,7 @@ almalinux:8.5-20211112
 EOS
 
 crane copy almalinux@sha256:08042694fffd61e6a0b3a22dadba207c8937977915ff6b1879ad744fd6638837 ${TEST_IMAGE}:almalinux-8
+
 
 cat <<EOS
 
@@ -27,6 +29,7 @@ EOS
 
 crane copy alpine@sha256:414e0518bb9228d35e4cd5165567fb91d26c6a214e9c95899e1e056fcd349011 ${TEST_IMAGE}:alpine-39
 	
+
 cat <<EOS
 
 ###########
@@ -37,6 +40,7 @@ EOS
 
 crane copy alpine@sha256:451eee8bedcb2f029756dc3e9d73bab0e7943c1ac55cff3a4861c52a0fdd3e98 ${TEST_IMAGE}:alpine-310
 
+
 cat <<EOS
 
 ###############################
@@ -46,6 +50,7 @@ ghcr.io/distroless/git:20220412
 EOS
 
 crane copy ghcr.io/distroless/git@sha256:107c3bcf9a5d92c88e1085cb949d247ebe95cfbf6235d4a4307d129d2874de71 ${TEST_IMAGE}:alpine-distroless
+
 
 cat <<EOS
 
@@ -58,3 +63,14 @@ EOS
 docker build -t ${TEST_IMAGE}:busybox-with-lockfile busybox-with-lockfile
 docker push ${TEST_IMAGE}:busybox-with-lockfile
 docker rmi -f ${TEST_IMAGE}:busybox-with-lockfile
+
+
+cat <<EOS
+
+############################################
+quay.io/fluentd_elasticsearch/fluentd:v2.9.0
+############################################
+
+EOS
+
+crane copy quay.io/fluentd_elasticsearch/fluentd@sha256:54716d825ec9791ffb403ac17a1e82159c98ac6161e02b2a054595ad01aa6726 ${TEST_IMAGE}:fluentd-multiple-lockfiles
