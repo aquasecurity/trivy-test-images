b0ed58c0711596e08d2b196fd0859ecd6255e57c
AMF <work@afdesk.com>
2022-06-07 12:06:46 +0600

    fix `gcr.io/distroless/base` image

--- push.sh
+++ push.sh
@@ -153,13 +153,13 @@ crane copy debian@sha256:94a5c04481bb50a4f34ebbb105e39388700202a6e34cb41b9b9afda
 
 cat <<EOS
 
-######################
-gcr.io/distroless/base
-######################
+##############################################################################################
+gcr.io/distroless/base@sha256:e37cf3289c1332c5123cbf419a1657c8dad0811f2f8572433b668e13747718f8
+##############################################################################################
 
-EOS
+EOS 
 
-crane copy gcr.io/distroless/base@sha256:6bf7a69660340caf6d227c9dc4ff5ca2028beb5f9280c05d3e4fe57c308be6ea ${TEST_IMAGE}:distroless-base
+crane copy gcr.io/distroless/base@sha256:e37cf3289c1332c5123cbf419a1657c8dad0811f2f8572433b668e13747718f8 ${TEST_IMAGE}:distroless-base
 
 cat <<EOS
 
